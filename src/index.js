f211e94e4fecde9047c055fb145266e5aafd6506
Allen He <allenhe77@gmail.com>
2022-09-12 17:41:13 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import _ from 'lodash'
-
+import _ from "lodash";
 
 function component() {
-
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  console.log(element);
   return element;
 }
 
