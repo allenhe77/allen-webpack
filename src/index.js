e551483fa6fce2f23df711fe07d74cad24529dc1
Allen He <allenhe77@gmail.com>
2022-09-12 17:49:59 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import _ from "lodash";
 
-function component() {
-  const element = document.createElement("div");
+// function component() {
+//   const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  console.log(element);
-  return element;
-}
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   console.log(element);
+//   return element;
+// }
 
 // document.body.appendChild(component());
 alert("cdn!");
